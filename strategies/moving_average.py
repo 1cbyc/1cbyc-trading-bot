8d2d26412fb88e250764ff228a8c3040e6b6249e
1cbyc <isaacnsisong@gmail.com>
2024-07-24 14:44:27 +0100

    updating moving average file with .iloc

--- strategies/moving_average.py
+++ strategies/moving_average.py
@@ -13,11 +13,13 @@ def moving_average_strategy(data, short_window, long_window):
     # data['Signal'][short_window:] = np.where(data['SMA_Short'][short_window:] > data['SMA_Long'][short_window:], 1, 0)
     #
     # i want to use .loc this time
-    data['Signal'] = 0
-    data.loc[short_window:, 'Signal'] = np.where(data['SMA_Short'][short_window:] > data['SMA_Long'][short_window:], 1, 0)
+    # data['Signal'] = 0
+    # data.loc[short_window:, 'Signal'] = np.where(data['SMA_Short'][short_window:] > data['SMA_Long'][short_window:], 1, 0)
 
+    # used .iloc for the next
+    data['Signal'] = 0
+    data.loc[data.index[short_window:], 'Signal'] = np.where(data['SMA_Short'].iloc[short_window:] > data['SMA_Long'].iloc[short_window:], 1, 0)
 
     # generate trading orders
     data['Position'] = data['Signal'].diff()
-
     return data
