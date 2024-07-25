380f13ef6846c6868e1c08e01b6d8cdb828790e9
1cbyc <isaacnsisong@gmail.com>
2024-07-26 00:18:56 +0100

    adjusted config.py to read from .env

--- crypto-trading/config.py
+++ crypto-trading/config.py
@@ -1,11 +1,25 @@
-# config.py
+# # config.py
+#
+# API_KEY = 'my_api_key'
+# SECRET_KEY = 'my_secret_key'
+#
+# EXCHANGE = 'binance'  # or any other exchange supported by ccxt
+# SYMBOL = 'BTC/USDT'
+# TIMEFRAME = '1h'
+# SHORT_WINDOW = 40
+# LONG_WINDOW = 100
+# TRADE_AMOUNT = 0.001  # depends on my requirements
 
-API_KEY = 'my_api_key'
-SECRET_KEY = 'my_secret_key'
+import os
+from dotenv import load_dotenv
 
-EXCHANGE = 'binance'  # or any other exchange supported by ccxt
-SYMBOL = 'BTC/USDT'
-TIMEFRAME = '1h'
-SHORT_WINDOW = 40
-LONG_WINDOW = 100
-TRADE_AMOUNT = 0.001  # Adjust based on your requirements
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+EXCHANGE = os.getenv('EXCHANGE')
+SYMBOL = os.getenv('SYMBOL')
+TIMEFRAME = os.getenv('TIMEFRAME')
+SHORT_WINDOW = int(os.getenv('SHORT_WINDOW'))
+LONG_WINDOW = int(os.getenv('LONG_WINDOW'))
+TRADE_AMOUNT = float(os.getenv('TRADE_AMOUNT'))
