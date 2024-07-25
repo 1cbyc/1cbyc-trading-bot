59b11367a1a3a39c5529668dcb2edf7af7d074ea
1cbyc <isaacnsisong@gmail.com>
2024-07-25 23:52:21 +0100

    config update

---
+++ crypto-trading/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+API_KEY = 'my_api_key'
+SECRET_KEY = 'my_secret_key'
+
+EXCHANGE = 'binance'  # or any other exchange supported by ccxt
+SYMBOL = 'BTC/USDT'
+TIMEFRAME = '1h'
+SHORT_WINDOW = 40
+LONG_WINDOW = 100
+TRADE_AMOUNT = 0.001  # Adjust based on your requirements
